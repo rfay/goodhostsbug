3cc9f89699d7295ce64fe9f93a037c7c5e65f82b
Randy Fay <randy@randyfay.com>
2023-04-06 10:04:37 -0600

    Change to do the HostsPerLine() after the Add, before flush

--- main.go
+++ main.go
@@ -3,16 +3,32 @@ package main
 import (
 	"fmt"
 	"github.com/goodhosts/hostsfile"
+	"os"
 )
 
 func main() {
+	if len(os.Args) != 3 {
+		fmt.Printf("Usage: %s <ip_address> <hostname>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	ip := os.Args[1]
+	hostname := os.Args[2]
+
 	hf, err := hostsfile.NewCustomHosts("/etc/hosts")
 	if err != nil {
 		panic(err)
 	}
 
+	err = hf.Add(ip, hostname)
+	if err != nil {
+		panic(err)
+	}
+
+	// If HostsPerLine() is called before hf.Add(), we get a panic on hf.Add().
 	hf.HostsPerLine(8)
-	err = hf.Add("127.0.0.1", "something.example.com")
+
+	err = hf.Flush()
 	if err != nil {
 		panic(err)
 	}
